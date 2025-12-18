6a30424f774c74a6e1577cd300651f65c7a5145a
AkanshaMittal <akanshamittal210@gmail.com>
2025-12-18 20:20:08 +0530

    loop questions

---
+++ Questions.js
@@ -0,0 +1,23 @@
+
+//average of numbers in an array
+
+// let marks=[85,97,44,37,76,60];
+// let averageMarks=0;
+// for(let i=0;i<marks.length;i++){
+//     averageMarks=marks[i]+averageMarks;
+// }
+// console.log(Math.floor(averageMarks/marks.length));
+
+
+//  -----------------------------------------------------------------------------
+// values of items  after 10% offer applied
+
+// let items=[250,645,300,900,50];
+//  let i=0;
+// for( let val of items){
+//     console.log(`value of item before offer applied`,val);
+//     let offer =val/10;
+//       items[i]=items[i]-offer;
+//      console.log("values after 10% offer applied",items[i]);
+//     i++;
+// }
