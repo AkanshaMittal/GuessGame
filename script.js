90c01eb459664b25fb44868868098b1ec881d385
AkanshaMittal <akanshamittal210@gmail.com>
2025-12-18 14:47:40 +0530

    chances mention

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-let gameNumber = 25;
-let userNumber = Number(prompt("Guess a number"));
+let userConsent = confirm("Do you want to play the game?");
 
-while (userNumber !== gameNumber) {
+if (userConsent) {
+    let chances = 1;
+    let GameNumber = 25;
 
-    if (userNumber < gameNumber) {
-        alert("Try a bigger number");
-    } else {
-        alert("Try a smaller number");
+while (chances <= 3) {
+        let userInput = prompt("Guess your number please");
+
+        if (userInput == GameNumber) {
+            alert("Congratulations! You guessed the correct number.");
+            break;
+        } else {
+            let message = `Wrong guess. This is your attempt ${chances} of 3.`;
+            if (chances === 2) {
+                message = `Wrong guess. This is your LAST attempt${chances} of 3.`;
+            }
+            alert(message);
+        } 
+        chances++;
+        if (chances === 3) {
+
+            alert(`Sorry, you've used all 3 attempts. The correct number was ${GameNumber}.`);
+
+            break;
+        }
     }
 
-    if (gameNumber % 2 === 0) {
-        alert("Hint: The correct number is even");
-    } else {
-        alert("Hint: The correct number is odd");
-    }
-
-    userNumber = Number(prompt("Guess a number"));
 }
-
-document.getElementById("result").innerText = "Congratulations! You guessed the correct number🎉🎉🎉🎉🎉🎉: " + gameNumber;
+ else 
+    {
+    alert("Maybe next time!");
+}
